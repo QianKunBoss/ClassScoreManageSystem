3fdee74139c9708e41acb474145be7220259fe8d
QianKunBoss <96124360+QianKunBoss@users.noreply.github.com>
2025-04-25 18:16:34 +0800

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -7,7 +7,7 @@ function showNav() {
     echo '
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
         <div class="container">
-            <a class="navbar-brand" href="../index.php">
+            <a class="navbar-brand" href="http://cxf.tianrld.top/index.php">
                 <i class="fas fa-chart-line"></i> 高2302班级操行分管理系统
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,22 +20,22 @@ function showNav() {
                 if (!in_array($currentPage, ['index.php', 'user_search.php'])) {
                 echo '
                     <li class="nav-item">
-                        <a class="nav-link" href="../pages/add_user.php">
+                        <a class="nav-link" href="http://cxf.tianrld.top/pages/add_user.php">
                             <i class="fas fa-user-plus"></i> 添加学生
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../pages/adjust_score.php">
+                        <a class="nav-link" href="http://cxf.tianrld.top/pages/adjust_score.php">
                             <i class="fas fa-edit"></i> 调整积分
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../pages/export.php">
+                        <a class="nav-link" href="http://cxf.tianrld.top/pages/export.php">
                             <i class="fas fa-edit"></i> 数据导出
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../pages/import_users.php">
+                        <a class="nav-link" href="http://cxf.tianrld.top/pages/import_users.php">
                             <i class="fas fa-file-import"></i> 批量导入
                         </a>
                     </li>';
@@ -43,7 +43,7 @@ function showNav() {
                 if (!in_array($currentPage, ['admin.php', 'user_detail.php', 'add_user.php', 'import_users.php', 'dengluye.php', 'adjust_score.php'])) {
                  echo '
                     <li class="nav-item">
-                        <a class="nav-link" href="../admin.php">
+                        <a class="nav-link" href="http://cxf.tianrld.top/admin.php">
                             <i class="fas fa-user-plus"></i> 后台管理
                         </a>
                     </li>';
@@ -75,6 +75,10 @@ function isLoggedIn() {
 // 显示页脚
 function showFooter() {
     echo '
+    <h6><br><br></h6>
+    <h6 Align="center">该项目已开源至Github仓库（开源协议：MIT）<br>
+    仓库地址：<a href="https://github.com/QianKunBoss/ClassScoreManageSystem">https://github.com/QianKunBoss/ClassScoreManageSystem</a><br>
+    该项目由Tianrld工作室成员开发</h6>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
     </html>';
